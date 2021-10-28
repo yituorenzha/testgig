2ae3878a1da27c43f07bc57f42860b0082826065
yituorenzha <68110685@qq.com>
2021-10-28 10:27:16 +0800

    myhtml

---
+++ 静态模板-html5模板网_files/ad_js.php
@@ -0,0 +1,3 @@
+<!--
+document.write("<li class=\"item\"><script type=\"text/javascript\">\r\n        (function() {\r\n            var s = \"_\" + Math.random().toString(36).slice(2);\r\n            document.write('<div style=\"\" id=\"' + s + '\"></div>');\r\n            (window.slotbydup = window.slotbydup || []).push({\r\n                id: \"u2307971\",\r\n                container: s\r\n            });\r\n        })();\r\n</script>\r\n<!-- 多条广告如下脚本只需引入一次 -->\r\n<script type=\"text/javascript\" src=\"//cpro.baidustatic.com/cpro/ui/c.js\" async=\"async\" defer=\"defer\" >\r\n</script>\r\n</li>\r\n");
+-->
